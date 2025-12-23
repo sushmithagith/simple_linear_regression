2b4db08249701b419a6b5e82875b7891101db4ca
sushmithagith <sushmithashanigaram3@gmail.com>
2025-12-23 11:39:11 +0530

    commited

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
